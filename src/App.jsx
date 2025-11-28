7aa6858fc0b31d3cae05cc3a1861431de7c42566
gowthamchowdary-k <kgowtham9284@gmail.com>
2025-11-28 18:23:25 +0530

    Add project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="hello">
+      <h1>Hello World</h1>
+      <p>This is my sample project for GitHub experiment.</p>
+    </div>
+  );
+}
+
+export default App;
